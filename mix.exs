899dc7d259bd0a0d6a82aaec2a3ee98e3ca68a21
Joe-noh <honzawa.j@pepabo.com>
2015-06-18 09:50:06 +0900

    ready to publish

--- mix.exs
+++ mix.exs
@@ -2,12 +2,17 @@ defmodule Pet.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :pet,
-     version: "0.1.1",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :pet,
+      version: "0.1.1",
+      elixir: "~> 1.0",
+      description: description,
+      package: package,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      docs: docs
+    ]
   end
 
   def application do
@@ -16,5 +21,22 @@ defmodule Pet.Mixfile do
 
   defp deps do
     [{:yomel, "~> 0.2"}]
+  end
+
+  defp description do
+    "Account management tool compatible with pit"
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     contributors: ["Joe Honzawa"],
+     licenses: ["MIT"],
+     links: %{
+       "GitHub" => "https://github.com/Joe-noh/pet"
+     }]
+  end
+
+  defp docs do
+    [readme: "README.md", main: "README"]
   end
 end
